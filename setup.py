878f03d4f597b9ca87f6ade00df605f2fb831245
Matteo Parrucci <parruc@gmail.com>
2016-09-19 03:35:01 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='parruc.violareggiocalabriamigration',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="Migration package for violareggiocalabria",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
